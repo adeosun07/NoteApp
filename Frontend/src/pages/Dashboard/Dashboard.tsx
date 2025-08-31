5a6160aa9fe5d79617d253203c628f9ea0facbca
Adeosun Oluwatimileyin <oluwatimileyinadeosun@gmil.com>
2025-08-31 16:12:55 -0700

    still getting the render on railway 2

--- Frontend/src/pages/Dashboard/Dashboard.tsx
+++ Frontend/src/pages/Dashboard/Dashboard.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import API from "../../Api";
 import LogoutButton from "../../components/LogoutButton";
 import reactLogo from "../../assets/react.svg";
