5d7e970c744ef3f8b714a3c53752af1ca930b0c4
Adeosun Oluwatimileyin <oluwatimileyinadeosun@gmil.com>
2025-08-31 15:13:23 -0700

    added global.d.ts

--- Frontend/src/components/OtpForm.tsx
+++ Frontend/src/components/OtpForm.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { useState } from "react";
 import { FaEye, FaEyeSlash } from "react-icons/fa";
 import styles from "../pages/SignupPage/SignupPage.module.css";
--- Frontend/src/global.d.ts
+++ Frontend/src/global.d.ts
@@ -1,22 +1,6 @@
-// src/global.d.ts
-
-// CSS Modules
-declare module '*.css' {
-  const content: { [className: string]: string };
-  export default content;
-}
-
-// SVG imports
+declare module '*.css';
+declare module '*.module.css';
 declare module '*.svg' {
   const content: string;
   export default content;
-}
-
-
-interface ImportMetaEnv {
-  readonly VITE_API_URL: string;
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv;
 }
