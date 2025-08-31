38f167d8fa7053a4278569f890b89af5a2cb5096
Adeosun Oluwatimileyin <oluwatimileyinadeosun@gmil.com>
2025-08-31 15:17:43 -0700

    added global.d.ts for a test

--- Frontend/src/global.d.ts
+++ Frontend/src/global.d.ts
@@ -4,3 +4,7 @@ declare module '*.svg' {
   const content: string;
   export default content;
 }
+/// <reference types="react" />
+/// <reference types="react-dom" />
+declare module 'react/jsx-runtime';
+declare module 'react-dom/client';
