e5596d7375c678424324ab171d5d69f2aba39f95
Adeosun Oluwatimileyin <oluwatimileyinadeosun@gmil.com>
2025-08-31 19:44:29 -0700

    redeploy to vercel 5

--- Backend/index.js
+++ Backend/index.js
@@ -2,6 +2,11 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import path from "path";
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 dotenv.config();
 const app = express();
